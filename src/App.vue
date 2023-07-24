55948b9b4387dbd413c7980373054b5785a7bc22
salazarmelita <asalazar@hyl.cl>
2023-07-24 05:01:14 -0400

    feat: first configuration

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
+import { ref, reactive } from 'vue'
 
-
-  // const monedas = ref([
-  //       { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-  //       { codigo: 'MXN', texto: 'Peso Mexicano'},
-  //       { codigo: 'EUR', texto: 'Euro'},
-  //       { codigo: 'GBP', texto: 'Libra Esterlina'},
-  //       { codigo: 'CLP', texto: 'Peso Chileno'},
-  // ])
+const monedas = ref([
+  { codigo: 'USD', texto: 'Dolar de Estados Unidos' },
+  { codigo: 'MXN', texto: 'Peso Mexicano' },
+  { codigo: 'EUR', texto: 'Euro' },
+  { codigo: 'GBP', texto: 'Libra Esterlina' },
+  { codigo: 'CLP', texto: 'Peso Chileno' },
+])
 
 
 </script> 
 
 <template>
-
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">-- Selecciona --</option>
+            <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }} </option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
